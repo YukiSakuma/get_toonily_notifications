d9ed7a6eae5f2fdc0c110cab01a013b62b958367
YukiSakuma <jeremy.miku@yahoo.com>
2020-02-24 18:27:00 +0800

    Update toonily.py

--- toonily.py
+++ toonily.py
@@ -64,8 +64,8 @@ def is_ended(link, num):
             ch_link = f"{link}chapter-{num}"
             print("The link of the chapter number you entered below is copied\n")
             print(ch_link)
-            ctypes.windll.user32.MessageBoxW(0, f"{ch_link}", f"Copying Chapter {num} close this "
-                                                              f"window to finish copy", 0)
+            ctypes.windll.user32.MessageBoxW(0, f"{ch_link} \nClose this window to finish complete copying the link", 
+                                             f"Copying Chapter {num}", 0)
             pyperclip.copy(ch_link)
         else:
             print(f"Invalid chapter {num}")
